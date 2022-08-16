729f0f0bb141a73f527b4fdfa060d10101facbd2
Daniel Gonçalves <daniel.goncalves@layer8.pt>
2022-08-16 12:15:07 +0100

    add translation

--- translate_gm.user.js
+++ translate_gm.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Translate GM
-// @version      0.8
+// @version      0.9
 // @description  Translate GM
 // @author		 Phost
 // @updateURL	 https://github.com/2phost/mh-gh-translator/raw/main/translate_gm.user.js
